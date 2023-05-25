4bde33532e9bb11108a29228b3787e1551d90a27
MinSeo <saysang2212@gmail.com>
2023-05-25 20:32:18 +0900

    test

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Todos from "./components/Todos";
+
+function App() {
+  return <Todos />;
+}
+
+export default App;
